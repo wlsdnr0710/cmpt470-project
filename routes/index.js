fb1bcc334a52ba48bfd5dc5d4a0bfa2589ad29d4
gskhosa <gskhosa@sfu.ca>
2020-11-06 15:06:27 -0800

    Fix rebase

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index');
 })
-
-router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-})
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/');
-}
 
 module.exports = router
