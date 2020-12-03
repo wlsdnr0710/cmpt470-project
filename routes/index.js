cea0b791df15e2db2828f4098c69b368dc061d71
Leo Qiu <qwleo1112@gmail.com>
2020-12-02 17:48:08 -0800

    testing

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,9 @@ router.get("/", function (req, res, next) {
 
     // go through all lists and accumulate unique ids
     var appearance = 0;
+    var most_wanted_game = 0;
+    var img = "";
+    var name = "";
     for (var key in dict) {
       if (dict[key] > appearance) {
         appearance = dict[key];
@@ -38,47 +41,68 @@ router.get("/", function (req, res, next) {
     Game.findOne({ appId: most_wanted_game }).exec((err, foundgame) => {
       if (err) return next(err);
       // console.log(" GAME", foundgame);
-      img =
-        "http://media.steampowered.com/steamcommunity/public/images/apps/" +
-        most_wanted_game +
-        "/" +
-        foundgame.imgLogoUrl +
-        ".jpg";
-      console.log(img);
-
-      User.find().exec((err, users) => {
+      if(foundgame){
+        img =
+          "http://media.steampowered.com/steamcommunity/public/images/apps/" +
+          most_wanted_game +
+          "/" +
+          foundgame.imgLogoUrl +
+          ".jpg";
+        console.log(img);
+      }
+      User.find().then(function(err, users){
+        console.log("users:",users);
         var dict = {};
-        for (var i = 0; i < users.length; i++) {
-          var x =users[i].numCompletedLists().then(function(result) {
-            // here you can use the result of promiseB
-            console.log(users[i]);
-            dict[users[i].username] = result;
-            console.log("user"+i+"'s lists completed: "+result);
-          })
-          .catch(console.log(err)) ;
-        }
+        var test = [];
+        // var counts = users.length;
+        // for (var i = 0; i < users.length; i++) {
+        //   console.log(user);
+        //   users[i].numCompletedLists()
+        //   .then(function(data) {
+        //     console.log("user"+i+"'s lists completed: "+data);
+        //     test.push(data);
+        //     dict[users[i].username] = data;
+        //     counts--;
+        //     if(counts==0){
+        //       console.log("EXIT");
+        //       var users_arr = Object.keys(dict).map(function(key) {
+        //         return [key, dict[key]];
+        //       });
+              
+        //       console.log("arr: ",users_arr);
+      
+        //       // Sort the array based on the second element
+        //       users_arr.sort(function(first, second) {
+        //         return second[1] - first[1];
+        //       });
+              
+        //       // Create a new array with only the first 5 items
+        //       console.log("test: ",test)
+        //       console.log("RESULT: ",users_arr);
+        //       top_five = users_arr.slice(0, 5);
+        //       console.log("RESULT: ",top_five);
+        //       // console.log("topfive: ",top_five);
+        //       if(foundgame){
+        //         name = foundgame.name;
+        //       }
+        //       else{
+        //         name = "none";
+        //       }
+      
 
-        var users_arr = Object.keys(dict).map(function(key) {
-          return [key, dict[key]];
-        });
-        
-        // Sort the array based on the second element
-        users_arr.sort(function(first, second) {
-          return second[1] - first[1];
-        });
-        
-        // Create a new array with only the first 5 items
-        top_five = users_arr.slice(0, 5);
-        console.log("RESULT: ",users_arr.slice(0, 5));
-
-
-        console.log("completed", top_five);
-        res.render("index", {
-          popular: most_wanted_game,
-          gamename: foundgame.name,
-          gameimage: img,
-          topfive: top_five,
-        });
+            // }
+                    //       console.log("completed", top_five);
+        //       res.render("index", {
+        //         popular: most_wanted_game,
+        //         gamename: name,
+        //         gameimage: img,
+        //         topfive: top_five,
+        //       })
+        //   })
+        //   .catch((err) => {
+        //     console.error(err);
+        //   });
+        // } 
       });
     });
   });
